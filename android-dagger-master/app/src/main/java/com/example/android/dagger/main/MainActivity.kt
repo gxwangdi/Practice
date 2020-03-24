81e3a0a68d8343d1447824eed767758811b6032a
gxwangdi <gxwangdi@gmail.com>
2020-03-24 00:24:28 -0700

    Setting up GoogleCodeLabs/android-dagger projects.
    git clone https://github.com/googlecodelabs/android-dagger
    
    https://codelabs.developers.google.com/codelabs/android-dagger

---
+++ android-dagger-master/app/src/main/java/com/example/android/dagger/main/MainActivity.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.dagger.main
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.dagger.MyApplication
+import com.example.android.dagger.R
+import com.example.android.dagger.login.LoginActivity
+import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.settings.SettingsActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainViewModel: MainViewModel
+
+    /**
+     * If the User is not registered, RegistrationActivity will be launched,
+     * If the User is not logged in, LoginActivity will be launched,
+     * else carry on with MainActivity
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val userManager = (application as MyApplication).userManager
+        if (!userManager.isUserLoggedIn()) {
+            if (!userManager.isUserRegistered()) {
+                startActivity(Intent(this, RegistrationActivity::class.java))
+                finish()
+            } else {
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            }
+        } else {
+            setContentView(R.layout.activity_main)
+
+            mainViewModel = MainViewModel(userManager.userDataRepository!!)
+            setupViews()
+        }
+    }
+
+    /**
+     * Updating unread notifications onResume because they can get updated on SettingsActivity
+     */
+    override fun onResume() {
+        super.onResume()
+        findViewById<TextView>(R.id.notifications).text = mainViewModel.notificationsText
+    }
+
+    private fun setupViews() {
+        findViewById<TextView>(R.id.hello).text = mainViewModel.welcomeText
+        findViewById<Button>(R.id.logout).setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
+    }
+}
