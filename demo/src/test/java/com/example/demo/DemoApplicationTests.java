4a33f742645167ff2e77e16e490a3e41ea2c0651
gxwangdi <gxwangdi@gmail.com>
2020-05-23 15:21:20 -0700

    Implemented test cases.
    For context loading and hello page rendering

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.demo.controllers.MvcController;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
 
+	@Autowired
+	private MvcController mvcController;
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
 	@Test
 	void contextLoads() {
+		assertThat(mvcController).isNotNull();
+	}
+	
+	@Test
+	void testHello() throws Exception {
+		mockMvc.perform(get("/hello"))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(content().string(containsString("Greetings message")));
 	}
 
 }
