79bb039ce73e32d8af0fd36eafdb3d1ab8f4bf68
gxwangdi <gxwangdi@gmail.com>
2020-05-24 13:31:07 -0700

    Added Product entity and ProductRepo.

---
+++ demo/src/main/java/com/example/demo/entities/Product.java
@@ -0,0 +1,57 @@
+package com.example.demo.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length = 64)
+	private String name;
+	
+	
+	private float price;
+	
+	public Product(String name, float price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+
+	public Product() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+}
---
+++ demo/src/main/java/com/example/demo/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.entities.Product;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+
+}
