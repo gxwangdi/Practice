fad6baeed6eec1c93fb7bae42bb2cdcbed8ab313
gxwangdi <gxwangdi@gmail.com>
2020-05-24 22:14:41 -0700

    Implemented jdbcTemplate query sample.
    Wrap up the JDBCTemplate demo.

---
+++ demo/src/main/java/com/example/demo/entities/Book.java
@@ -0,0 +1,56 @@
+package com.example.demo.entities;
+
+public class Book {
+
+	private Integer id;
+	private String title;
+	private String author;
+	private float price;
+	
+	public Book(Integer id, String title, String author, float price) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.price = price;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", price=" + price + "]";
+	}
+	
+	
+}
--- demo/src/main/java/com/example/demo/utils/SpringJdbcTemplateDemo.java
+++ demo/src/main/java/com/example/demo/utils/SpringJdbcTemplateDemo.java
@@ -1,7 +1,14 @@
 package com.example.demo.utils;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import com.example.demo.entities.Book;
 
 /**
  * A Simple Java application for JdbcTemplate demo. 
@@ -18,10 +25,32 @@ public class SpringJdbcTemplateDemo {
 		
 		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 		
-		String sql = "INSERT INTO books(title, author, price) VALUES(?,?,?)";
-		int result = jdbcTemplate.update(sql, "Counter Hack Reloaded", "Skoudis", 17.96f);
-		if (result >0) {
-			System.out.println(result + " books are inserted");
+//		String sql = "INSERT INTO books(title, author, price) VALUES(?,?,?)";
+//		int result = jdbcTemplate.update(sql, "Counter Hack Reloaded", "Skoudis", 17.96f);
+//		if (result >0) {
+//			System.out.println(result + " books are inserted");
+//		}
+		
+		String sql = "SELECT * FROM books";
+		RowMapper<Book> rowMapper = new RowMapper<Book>() {
+			public Book mapRow(ResultSet result, int row) throws SQLException {
+				Integer id = result.getInt("book_id");
+				String title = result.getString("title");
+				String author = result.getString("author");
+				float price = result.getFloat("price");
+				
+				return new Book(id, title, author, price);
+			}
+		};
+		List<Book> books = jdbcTemplate.query(sql, rowMapper);
+		for (Book book : books) {
+			System.out.println(book);
+		}
+		
+		sql = "SELECT * FROM books WHERE book_id <4";
+		books = jdbcTemplate.query(sql, rowMapper);
+		for (Book book : books) {
+			System.out.println(book);
 		}
 	}
 
